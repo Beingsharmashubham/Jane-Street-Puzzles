1a157392a3988e033040b12d48c031eda7402fff
Shubham Sharma <Beingsharmashubham@gmail.com>
2025-09-07 03:24:53 +0530

    solution.py

--- 2021/january/solution.py
+++ 2021/january/solution.py
@@ -1,6 +1,11 @@
 ﻿print('Hello Jane Street Puzzle 2021 January')
-print('Problem Statement: Jane received 78 figurines as gifts this holiday season: 12 drummers drumming, 11 pipers piping, 10 lords a-leaping, etc., down to 1 partridge in a pear tree. 
-      They are all mixed together in a big bag. She agrees with her friend Alex that this seems like too many figurines for one person to have, so she decides to give some of her figurines to Alex. 
-      Jane will uniformly randomly pull figurines out of the bag one at a time until she pulls out the partridge in a pear tree, and will give Alex all of the figurines she pulled out of the bag (except the partridge, that’s Jane’s favorite).
-      If n is the maximum number of any one type of ornament that Alex gets, what is the expected value of n, to seven significant figures?)
+""" 
+Problem Statement: 
+
+Jane received 78 figurines as gifts this holiday season: 12 drummers drumming, 11 pipers piping, 10 lords a-leaping, etc., down to 1 partridge in a pear tree. 
+They are all mixed together in a big bag. She agrees with her friend Alex that this seems like too many figurines for one person to have, so she decides to give some of her figurines to Alex. 
+Jane will uniformly randomly pull figurines out of the bag one at a time until she pulls out the partridge in a pear tree, and will give Alex all of the figurines she pulled out of the bag (except the partridge, that’s Jane’s favorite).
+If n is the maximum number of any one type of ornament that Alex gets, what is the expected value of n, to seven significant figures?
+"""
+
 
